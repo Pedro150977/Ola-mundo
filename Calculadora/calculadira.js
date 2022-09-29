7f18d9694415302649198ab1aca773cc7fee0334
Pedro <pvictorgouveia@gmail.com>
2022-09-29 16:41:53 -0300

    Comentário
    Coloquei comentário em duas linhas do meu código.

--- Calculadora/calculadira.js
+++ Calculadora/calculadira.js
@@ -9,13 +9,14 @@ function limpar() {
 
 function back() {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1); 
+    // o substring + o nome da variável.length - 1 é usado para deletar somente um número.
 }
 
 function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
     if (resultado) {
-        document.getElementById('resultado').innerHTML = eval(resultado); 
+        document.getElementById('resultado').innerHTML = eval(resultado); // a função eval faz os calculos
     } else {
         alert('Por favor insira um número')
     }
